989d1e0ea945655c2748c63300e552f31a27e426
Emre <e@emre.pm>
2025-08-10 18:40:24 +0300

    Add opencode

--- stow/nvim/.config/nvim/lua/plugins/coding.lua
+++ stow/nvim/.config/nvim/lua/plugins/coding.lua
@@ -96,4 +96,21 @@ return {
       { "<leader>zz", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
     },
   },
+  {
+    "sudo-tee/opencode.nvim",
+    config = function()
+      require("opencode").setup({})
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          anti_conceal = { enabled = false },
+          file_types = { "markdown", "opencode_output" },
+        },
+        ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
+      },
+    },
+  },
 }

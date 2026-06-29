35d65ef71fd891503bb2fcfa444e29c06dc6bc45
Emre <e@emre.pm>
2026-06-29 19:09:46 +0300

    chore: Remove Copilot from nvim completion

--- stow/nvim/.config/nvim/lua/plugins/cmp.lua
+++ stow/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,5 +1,3 @@
-local helpers = require("config.helpers")
-
 return {
   {
     "hrsh7th/nvim-cmp",
@@ -86,45 +84,6 @@ return {
         source.group_index = source.group_index or 1
       end
       require("cmp").setup(opts)
-    end,
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          helpers.lsp.on_attach(function(client)
-            if client.name == "copilot" then
-              copilot_cmp._on_insert_enter({})
-            end
-          end)
-        end,
-      },
-    },
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 100,
-      })
     end,
   },
 }
